4917d790fed723434340a164a12ed63d90674dab
sleeplesseditor <josh@bryson.me.uk>
2019-01-10 01:17:00 +0000

    Search function completed

--- src/context.js
+++ src/context.js
@@ -4,10 +4,24 @@ import axios from 'axios';
 const Context = React.createContext();
 const musicMatch = require('./config/keys').MM_API;
 
+const reducer = (state, action) => {
+    switch(action.type){
+        case 'SEARCH_TRACKS':
+            return {
+                ...state,
+                track_list: action.payload,
+                heading: 'Search Results'
+            };
+            default:
+                return state;
+    }
+}
+
 export class Provider extends Component {
     state = {
         track_list: [],
-        heading: 'Top 10 Tracks'
+        heading: 'Top 10 Tracks',
+        dispatch: action => this.setState(state => reducer(state, action))
     };
     
     //Country code and track numbers can be changed here
